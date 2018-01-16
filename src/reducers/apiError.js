152e255df3216155eb243f9ba08a969aee52a90c
Just RAG <justrag@gmail.com>
2018-01-16 22:18:13 +0100

    STORY_FAILURE correction

--- src/reducers/apiError.js
+++ src/reducers/apiError.js
@@ -1,9 +1,13 @@
-import { GET_LIST_REQUEST, GET_LIST_FAILURE } from '../constants/';
+import {
+  GET_LIST_REQUEST,
+  GET_LIST_FAILURE,
+  GET_STORY_FAILURE
+} from '../constants/';
 const apiError = (state = false, { type, apiError }) => {
   switch (type) {
     case GET_LIST_REQUEST:
       return false;
-    case GET_LIST_FAILURE:
+    case GET_LIST_FAILURE || GET_STORY_FAILURE:
       return apiError;
     default:
       return state;
